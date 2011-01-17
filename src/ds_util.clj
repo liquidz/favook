243f7daa677a382e2b9d06ff475789e256d0b1ab
liquidz <liquidz.uo@gmail.com>
2011-01-18 01:29:00 +0900

    udate entity structure

--- src/ds_util.clj
+++ src/ds_util.clj
@@ -10,4 +10,5 @@
 (defn key? [obj] (instance? Key obj))
 (defn key->str [obj] (if (key? obj) (KeyFactory/keyToString obj)))
 (defn str->key [obj] (if (string? obj) (KeyFactory/stringToKey obj)))
-(defn entity? [obj] (instance? (:on-interface ds/EntityProtocol) obj))
+;(defn entity? [obj] (instance? (:on-interface ds/EntityProtocol) obj))
+(defn entity? [obj] (extends? ds/EntityProtocol (class obj)))
--- test/favook/test/core.clj
+++ test/favook/test/core.clj
@@ -14,25 +14,22 @@
 
 (use-fixtures :each (ae-testing/local-services :all))
 
+(defn- foreach [f l] (doseq [x l] (f x)))
+
 (ds/defentity TestEntity [^:key a b])
 (ds/defentity SubEntity [^:key c d])
-
-(defn print-entities [obj]
-  (if (or (seq? obj) (list? obj))
-    (doseq [e obj] (print-entities e))
-    (println (str "[" (type obj) "]: " obj))
-    )
-  )
 
 (deftest test-entity?
   (is (entity? (TestEntity. "a" "b")))
   )
 
 
-(ds/defentity User [^:key uid name login-type secret-mail])
-(ds/defentity Book [^:key title isbn author])
+;(ds/defentity User [^:key name login-type secret-mail])
+(ds/defentity User [^:key name secret-mail point date])
+(ds/defentity Book [^:key title author isbn])
 (ds/defentity Favorite [book user point date])
 (ds/defentity Comment [book user text date])
+(ds/defentity Activity [book user message date])
 
 ; {{{
 ;(defn bytes->hex-str [bytes]
@@ -47,28 +44,44 @@
 ;(def str->sha1 (partial digest-hex "SHA1"))
 ; }}}
 
-(deftest test-hoge
-  (let [alice (User. 1 "Alice" "twitter" "")
-        bob (User. 2 "Bob" "guest" "")
-        masa (User. 3 "masa" "facebook" "")
-        book (Book. "title" "isbn" "neko")
-        book2 (Book. "hoge" "fuga" "inu")
-        book3 (Book. "uo" "chan" "aaa")
-        fav (Favorite. book alice 2 (Date.))
-        fav2 (Favorite. book bob 1 (Date.))
-        fav3 (Favorite. book2 alice 1 (Date.))
-        fav4 (Favorite. book3 masa 1 (Date.))
+(deftest test-user
+  (let [u1 (User. "Alice" "")
+        u2 (User. "Bob" "")
+        b1 (Book. "aaa" "bbb" "")
+        b2 (Book. "ccc" "ddd" "")
+        b3 (Book. "eee" "fff" "")
         ]
-    (ds/save! [alice bob book book2 book3 fav fav2 fav3 fav4])
+    (ds/save! [u1 u2 b1 b2 b3
+               (Favorite. b1 u1 2 (Date.))
+               (Favorite. b1 u2 1 (Date.))
+               (Favorite. b2 u1 1 (Date.))
+               (Favorite. b3 u2 1 (Date.))
+               (Comment. b1 u1 "zzz" (Date.))
+               (Comment. b2 u1 "yyy" (Date.))
+               (Comment. b1 u2 "xxx" (Date.))
+               (Comment. b3 u2 "www" (Date.))
+               ])
 
-    ;(println (ds/query :kind Favorite :filter (= :user alice)))
-
-    ;(doseq [x (conj (ds/query :kind Favorite :filter (= :user alice))
-    ;                (ds/query :kind Favorite :filter (= :user bob)))]
-    ;  (println x)
-    ;  )
-    (doseq [x (ds/query :kind Favorite :filter (= :user alice) :sort [:point])]
-      (println x)
-      )
+    (ds/query :kind Favorite )
     )
   )
+
+;(deftest test-hoge
+;  (let [alice (User. 1 "Alice" "twitter" "")
+;        bob (User. 2 "Bob" "guest" "")
+;        masa (User. 3 "masa" "facebook" "")
+;        book (Book. "title" "isbn" "neko")
+;        book2 (Book. "hoge" "fuga" "inu")
+;        book3 (Book. "uo" "chan" "aaa")
+;        fav (Favorite. book alice 2 (Date.))
+;        fav2 (Favorite. book bob 1 (Date.))
+;        fav3 (Favorite. book2 alice 1 (Date.))
+;        fav4 (Favorite. book3 masa 1 (Date.))
+;        ]
+;    (ds/save! [alice bob book book2 book3 fav fav2 fav3 fav4])
+;
+;    ;(doseq [x (ds/query :kind Favorite :filter (= :user alice) :sort [:point])]
+;    ;  (println x)
+;    ;  )
+;    )
+;  )
